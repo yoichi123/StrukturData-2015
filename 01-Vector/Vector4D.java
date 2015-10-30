98b052cfae55e0974e3d4fbaf88ef11c6246cf29
putra arifah <putraarifah@gmail.com>
2015-10-30 17:51:24 +0700

    UnitTest03

---
+++ 01-Vector/Vector4D.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Vector4D extends Vector3D {
+    private Date t;
+   public Vector4D (double x, double y, double z, Date t){
+       super(x,y,z);
+       this.t=t;
+    }
+    Date getT(){
+       return t; 
+    }
+}
