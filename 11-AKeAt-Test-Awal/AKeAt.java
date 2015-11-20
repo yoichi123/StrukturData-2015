8a0b86826fdb7d0152991b16770d4d1495b90301
putra arifah <putraarifah@gmail.com>
2015-11-20 15:09:28 +0700

    testawal

---
+++ 11-AKeAt-Test-Awal/AKeAt.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AKeAt {
+    public void aKeAt(String sumber, String sasaran) throws IOException{
+        FileInputStream masukan = null;
+        FileOutputStream keluaran = null;
+        
+        try{
+            masukan = new FileInputStream(sumber);
+            keluaran = new FileOutputStream(sasaran);
+            
+            int karakter = masukan.read();
+            while (karakter != -1){
+                if(karakter=='A' || karakter=='a'){
+                    karakter='@';
+                }
+                keluaran.write(karakter);
+                karakter = masukan.read();
+            }
+            keluaran.flush();
+        }
+        
+        catch (IOException kesalahan) {
+            System.out.printf("Terjadi kesalahan: %s", kesalahan);
+        }
+        
+        finally {
+            if (masukan != null)
+                masukan.close();
+            if (keluaran != null)
+                keluaran.close();
+        }
+    }
+}
